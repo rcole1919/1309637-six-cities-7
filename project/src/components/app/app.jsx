a31d4b3abc15127af86b0d8da6cfa2a39eafd60d
rolan <roland.alex@mail.ru>
2021-06-02 22:20:07 +0400

    add page and card component

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Page from '../page/page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({cards, placesCount}) {
+  return (
+    <Page cards={cards} placesCount={placesCount} />
+  );
 }
+
+App.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ),
+  placesCount :PropTypes.number.isRequired,
+};
 
 export default App;
